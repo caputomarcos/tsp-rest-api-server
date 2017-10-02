5349281e0000d6616dddce0871398f80a1b63a68
caputomarcos <caputo.marcos@gmail.com>
2017-10-01 22:15:10 -0300

    fix version

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ mongo_requires = [
 
 setup(
     name='tsp_rest_api_server',
-    version='v3.4',
+    version='v3.5',
     author='Marcos Caputo <caputo.marcos@gmail.com>',
     url='https://github.com/caputomarcos/tsp-rest-api-server',
     description="TSP Rest Api Server - Rest Api Server using Dijsktra's algorithm applied to travelling salesman problem.",
